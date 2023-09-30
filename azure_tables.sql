82b2dc535f73296d2b7da53c70ce1ca5e4821652
Vinay Kumar M <45409524+vinaykm5758@users.noreply.github.com>
2023-09-30 13:04:17 -0400

    Add files via upload

---
+++ azure_tables.sql
@@ -0,0 +1,366 @@
+
+
+
+
+
+DROP TABLE [AZURE_DWH].[SalesOrderDetail];
+
+CREATE TABLE [AZURE_DWH].[SalesOrderDetail](
+	[SalesOrderID] [int] NOT NULL,
+	[SalesOrderDetailID] [int] NOT NULL,
+	[OrderQty] [smallint] NOT NULL,
+	[ProductID] [int] NOT NULL,
+	[UnitPrice] [money] NOT NULL,
+	[UnitPriceDiscount] [money] NOT NULL,
+	[LineTotal]  [money] NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_SalesOrderDetail_SalesOrderID_SalesOrderDetailID] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderID] ASC,
+	[SalesOrderDetailID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_SalesOrderDetail_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+----
+
+----
+
+CREATE TABLE AZURE_DWH.[SalesOrderHeader](
+	[SalesOrderID] [int]  NOT NULL,
+	[RevisionNumber] [tinyint] NOT NULL,
+	[OrderDate] [datetime] NOT NULL,
+	[DueDate] [datetime] NOT NULL,
+	[ShipDate] [datetime] NULL,
+	[Status] [tinyint] NOT NULL,
+	[OnlineOrderFlag] [nvarchar](50) NULL,
+	[SalesOrderNumber]  [nvarchar](50)  NULL,
+	[PurchaseOrderNumber] [nvarchar](50) NULL,
+	[AccountNumber] [nvarchar](50) NULL,
+	[CustomerID] [int] NOT NULL,
+	[ShipToAddressID] [int] NULL,
+	[BillToAddressID] [int] NULL,
+	[ShipMethod] [nvarchar](50) NOT NULL,
+	[CreditCardApprovalCode] [varchar](15) NULL,
+	[SubTotal] [money] NOT NULL,
+	[TaxAmt] [money] NOT NULL,
+	[Freight] [money] NOT NULL,
+	[TotalDue]  [money]  NULL,
+	[Comment] [nvarchar](max) NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_SalesOrderHeader_SalesOrderID] PRIMARY KEY CLUSTERED 
+(
+	[SalesOrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_SalesOrderHeader_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_SalesOrderHeader_SalesOrderNumber] UNIQUE NONCLUSTERED 
+(
+	[SalesOrderNumber] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+-----
+
+
+CREATE TABLE [AZURE_DWH].[ProductModelProductDescription](
+	[ProductModelID] [int] NOT NULL,
+	[ProductDescriptionID] [int] NOT NULL,
+	[Culture] [nchar](6) NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_ProductModelProductDescription_ProductModelID_ProductDescriptionID_Culture] PRIMARY KEY CLUSTERED 
+(
+	[ProductModelID] ASC,
+	[ProductDescriptionID] ASC,
+	[Culture] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_ProductModelProductDescription_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+-----
+
+drop table AZURE_DWH.[ProductModel];
+
+CREATE TABLE AZURE_DWH.[ProductModel](
+	[ProductModelID] [int]  NOT NULL,
+	[Name] VARCHAR(100) NULL,
+	[CatalogDescription] xml NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_ProductModel_ProductModelID] PRIMARY KEY CLUSTERED 
+(
+	[ProductModelID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_ProductModel_Name] UNIQUE NONCLUSTERED 
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_ProductModel_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+----------
+
+drop table AZURE_DWH.ProductDescription;
+
+CREATE TABLE AZURE_DWH.[ProductDescription](
+	[ProductDescriptionID] [int]  NOT NULL,
+	[Description] [nvarchar](400) NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_ProductDescription_ProductDescriptionID] PRIMARY KEY CLUSTERED 
+(
+	[ProductDescriptionID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_ProductDescription_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+------
+
+
+drop table  AZURE_DWH.[ProductCategory];
+
+
+CREATE TABLE AZURE_DWH.[ProductCategory](
+	[ProductCategoryID] [int]  NOT NULL,
+	[ParentProductCategoryID] [int] NULL,
+	[Name] VARCHAR(255) NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_ProductCategory_ProductCategoryID] PRIMARY KEY CLUSTERED 
+(
+	[ProductCategoryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_ProductCategory_Name] UNIQUE NONCLUSTERED 
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_ProductCategory_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+---
+
+
+drop table AZURE_DWH.Product;
+
+CREATE TABLE [AZURE_DWH].[Product](
+	[ProductID] [int]  NOT NULL,
+	[Name] VARCHAR(200) NULL,
+	[ProductNumber] [nvarchar](25) NOT NULL,
+	[Color] [nvarchar](15) NULL,
+	[StandardCost] [money] NOT NULL,
+	[ListPrice] [money] NOT NULL,
+	[Size] [nvarchar](5) NULL,
+	[Weight] [decimal](8, 2) NULL,
+	[ProductCategoryID] [int] NULL,
+	[ProductModelID] [int] NULL,
+	[SellStartDate] [datetime] NOT NULL,
+	[SellEndDate] [datetime] NULL,
+	[DiscontinuedDate] [datetime] NULL,
+	[ThumbNailPhoto] [varbinary](max) NULL,
+	[ThumbnailPhotoFileName] [nvarchar](50) NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Product_ProductID] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_Product_Name] UNIQUE NONCLUSTERED 
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_Product_ProductNumber] UNIQUE NONCLUSTERED 
+(
+	[ProductNumber] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_Product_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+--
+
+drop table  [AZURE_DWH].[CustomerAddress];
+
+
+CREATE TABLE [AZURE_DWH].[CustomerAddress](
+	[CustomerID] [int] NOT NULL,
+	[AddressID] [int] NOT NULL,
+	[AddressType] VARCHAR(200) NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_CustomerAddress_CustomerID_AddressID] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC,
+	[AddressID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_CustomerAddress_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+-----
+
+drop table  [AZURE_DWH].[Customer];
+
+CREATE TABLE AZURE_DWH.[Customer](
+	[CustomerID] [int] NOT NULL,
+	[NameStyle] VARCHAR(100) NOT NULL,
+	[Title] [nvarchar](8) NULL,
+	[FirstName] VARCHAR(100) NULL,
+	[MiddleName] VARCHAR(100) NULL,
+	[LastName] VARCHAR(100) NOT NULL,
+	[Suffix] [nvarchar](10) NULL,
+	[CompanyName] [nvarchar](128) NULL,
+	[SalesPerson] [nvarchar](256) NULL,
+	[EmailAddress] [nvarchar](50) NULL,
+	[Phone] VARCHAR(100) NULL,
+	[PasswordHash] [varchar](128) NOT NULL,
+	[PasswordSalt] [varchar](10) NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Customer_CustomerID] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_Customer_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+
+
+--
+
+drop table AZURE_DWH.Address
+
+CREATE TABLE AZURE_DWH.[Address](
+	[AddressID] [int]  NOT NULL,
+	[AddressLine1] [nvarchar](60) NOT NULL,
+	[AddressLine2] [nvarchar](60) NULL,
+	[City] [nvarchar](30) NOT NULL,
+	[StateProvince] VARCHAR(100) NULL,
+	[CountryRegion]  VARCHAR(100) NULL,
+	[PostalCode] [nvarchar](15) NOT NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Address_AddressID] PRIMARY KEY CLUSTERED 
+(
+	[AddressID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [AK_Address_rowguid] UNIQUE NONCLUSTERED 
+(
+	[rowguid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
+----
+
+
+
+
+
+
+SELECT * FROM AZURE_DWH.Address;
+SELECT * FROM AZURE_DWH.Customer;
+SELECT * FROM AZURE_DWH.CustomerAddress;
+SELECT * FROM AZURE_DWH.Product;
+SELECT * FROM AZURE_DWH.ProductCategory;
+SELECT * FROM AZURE_DWH.ProductDescription;
+SELECT * FROM AZURE_DWH.ProductModel;
+SELECT * FROM AZURE_DWH.ProductModelProductDescription;
+SELECT * FROM AZURE_DWH.SalesOrderDetail;
+SELECT * FROM AZURE_DWH.SalesOrderHeader;
+
+
+
+
+SELECT COUNT(*) FROM AZURE_DWH.Address;
+SELECT COUNT(*) FROM AZURE_DWH.Customer;
+SELECT COUNT(*) FROM AZURE_DWH.CustomerAddress;
+SELECT COUNT(*) FROM AZURE_DWH.Product;
+SELECT COUNT(*) FROM AZURE_DWH.ProductCategory;
+SELECT COUNT(*) FROM AZURE_DWH.ProductDescription;
+SELECT COUNT(*) FROM AZURE_DWH.ProductModel;
+SELECT COUNT(*) FROM AZURE_DWH.ProductModelProductDescription;
+SELECT COUNT(*) FROM AZURE_DWH.SalesOrderDetail;
+SELECT COUNT(*) FROM AZURE_DWH.SalesOrderHeader
+
+
+
+
+
+
+TRUNCATE TABLE AZURE_DWH.Address;
+TRUNCATE TABLE AZURE_DWH.Customer;
+TRUNCATE TABLE AZURE_DWH.CustomerAddress;
+TRUNCATE TABLE AZURE_DWH.Product;
+TRUNCATE TABLE AZURE_DWH.ProductCategory;
+TRUNCATE TABLE AZURE_DWH.ProductDescription;
+TRUNCATE TABLE AZURE_DWH.ProductModel;
+TRUNCATE TABLE AZURE_DWH.ProductModelProductDescription;
+TRUNCATE TABLE AZURE_DWH.SalesOrderDetail;
+TRUNCATE TABLE AZURE_DWH.SalesOrderHeader;
+
+
+
+
+
+
+
+
+
+
+SELECT COUNT(*) FROM AZURE_DWH.Address;
+SELECT COUNT(*) FROM AZURE_DWH.Customer;
+SELECT COUNT(*) FROM AZURE_DWH.CustomerAddress;
+SELECT COUNT(*) FROM AZURE_DWH.Product;
+SELECT COUNT(*) FROM AZURE_DWH.ProductCategory;
+SELECT COUNT(*) FROM AZURE_DWH.ProductDescription;
+SELECT COUNT(*) FROM AZURE_DWH.ProductModel;
+SELECT COUNT(*) FROM AZURE_DWH.ProductModelProductDescription;
+SELECT COUNT(*) FROM AZURE_DWH.SalesOrderDetail;
+SELECT COUNT(*) FROM AZURE_DWH.SalesOrderHeader
+
